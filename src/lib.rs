a4afd6323fd0091c8cce507a3e5a9969c36bfc2c
Zackary Troop <zack.tro@gmail.com>
2023-04-08 21:40:54 -0400

    Add LRU cache

--- src/lib.rs
+++ src/lib.rs
@@ -30,8 +30,8 @@ where
     ///
     /// # Arguments
     ///
-    /// * `key` - The key to store in the cache.
-    /// * `value` - The value to store in the cache.
+    /// * `key` - The key of the entry to be inserted.
+    /// * `value` - The value of the entry to be inserted.
     pub fn insert(&mut self, key: K, value: V) {
         let expires_at = Utc::now() + self.ttl;
         self.cache.insert(key, (value, expires_at));
@@ -94,7 +94,7 @@ where
 pub struct FIFOCache<K, V> {
     capacity: usize,
     keys: VecDeque<K>,
-    values: HashMap<K, V>,
+    cache: HashMap<K, V>,
 }
 
 impl<K: Clone + Eq + std::hash::Hash, V> FIFOCache<K, V> {
@@ -107,7 +107,7 @@ impl<K: Clone + Eq + std::hash::Hash, V> FIFOCache<K, V> {
         FIFOCache {
             capacity,
             keys: VecDeque::with_capacity(capacity),
-            values: HashMap::new(),
+            cache: HashMap::new(),
         }
     }
 
@@ -116,29 +116,94 @@ impl<K: Clone + Eq + std::hash::Hash, V> FIFOCache<K, V> {
     ///
     /// # Arguments
     ///
-    /// * `key` - The key associated with the value.
-    /// * `value` - The value to be stored in the cache.
+    /// * `key` - The key of the entry to be inserted.
+    /// * `value` - The value of the entry to be inserted.
     pub fn insert(&mut self, key: K, value: V) {
-        if self.values.len() == self.capacity {
+        if self.cache.len() == self.capacity {
             if let Some(removed_key) = self.keys.pop_front() {
-                self.values.remove(&removed_key);
+                self.cache.remove(&removed_key);
             }
         }
         self.keys.push_back(key.clone());
-        self.values.insert(key, value);
+        self.cache.insert(key, value);
     }
 
     /// Retrieves the value associated with the given key from the cache.
     ///
     /// # Arguments
     ///
-    /// * `key` - The key associated with the value.
+    /// * `key` - The key to retrieve from the cache.
     ///
     /// # Returns
     ///
     /// An option containing a reference to the value if found, otherwise None.
     pub fn get(&self, key: &K) -> Option<&V> {
-        self.values.get(key)
+        self.cache.get(key)
+    }
+}
+
+/// An implementation of an LRU (Least Recently Used) Cache.
+///
+/// The cache has a maximum capacity and automatically removes
+/// the least recently used entry when the capacity is exceeded.
+pub struct LRUCache<K, V> {
+    capacity: usize,
+    cache: HashMap<K, (V, usize)>,
+    lru_keys: VecDeque<K>,
+}
+
+impl<K: Clone + Eq + std::hash::Hash, V> LRUCache<K, V> {
+    /// Creates a new LRUCache with the specified capacity.
+    ///
+    /// # Arguments
+    ///
+    /// * `capacity` - The maximum number of entries the cache can hold.
+    pub fn new(capacity: usize) -> Self {
+        LRUCache {
+            capacity,
+            cache: HashMap::with_capacity(capacity),
+            lru_keys: VecDeque::with_capacity(capacity),
+        }
+    }
+
+    /// Retrieves the value associated with the given key if it exists, updating the LRU order.
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - The key to retrieve from the cache.
+    ///
+    /// # Returns
+    ///
+    /// * `Some(&V)` - If the key exists in the cache.
+    /// * `None` - If the key is not in the cache.
+    pub fn get(&mut self, key: &K) -> Option<&V> {
+        if let Some((value, index)) = self.cache.get_mut(key) {
+            self.lru_keys.remove(*index);
+            self.lru_keys.push_back(key.clone());
+            let new_index = self.lru_keys.len() - 1;
+            *index = new_index;
+            Some(value)
+        } else {
+            None
+        }
+    }
+
+    /// Inserts a key-value pair into the cache, removing the least recently used entry if needed.
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - The key of the entry to be inserted.
+    /// * `value` - The value of the entry to be inserted.
+    pub fn insert(&mut self, key: K, value: V) {
+        if self.cache.len() >= self.capacity {
+            if let Some(removed_key) = self.lru_keys.pop_front() {
+                self.cache.remove(&removed_key);
+            }
+        }
+
+        let index = self.lru_keys.len();
+        self.lru_keys.push_back(key.clone());
+        self.cache.insert(key, (value, index));
     }
 }
 
@@ -150,16 +215,16 @@ mod tests {
     #[test]
     fn test_ttl_cache() {
         let mut cache = TtlCache::new(Duration::milliseconds(100));
-        cache.insert("key1", "value1");
-        cache.insert("key2", "value2");
+        cache.insert("key1", 1);
+        cache.insert("key2", 2);
 
-        assert_eq!(cache.get(&"key1"), Some("value1"));
-        assert_eq!(cache.get(&"key2"), Some("value2"));
+        assert_eq!(cache.get(&"key1"), Some(1));
+        assert_eq!(cache.get(&"key2"), Some(2));
 
         sleep(std::time::Duration::from_millis(50));
         cache.clear_expired();
-        assert_eq!(cache.get(&"key1"), Some("value1"));
-        assert_eq!(cache.get(&"key2"), Some("value2"));
+        assert_eq!(cache.get(&"key1"), Some(1));
+        assert_eq!(cache.get(&"key2"), Some(2));
 
         sleep(std::time::Duration::from_millis(60));
         cache.clear_expired();
@@ -171,14 +236,31 @@ mod tests {
     fn test_fifo_cache() {
         let mut cache = FIFOCache::new(2);
 
-        cache.insert(1, "one");
-        cache.insert(2, "two");
-        assert_eq!(cache.get(&1), Some(&"one"));
-        assert_eq!(cache.get(&2), Some(&"two"));
+        cache.insert("one", 1);
+        cache.insert("two", 2);
+        assert_eq!(cache.get(&"one"), Some(&1));
+        assert_eq!(cache.get(&"two"), Some(&2));
 
-        cache.insert(3, "three"); // Evicts the first entry (1, "one").
-        assert_eq!(cache.get(&1), None);
-        assert_eq!(cache.get(&2), Some(&"two"));
-        assert_eq!(cache.get(&3), Some(&"three"));
+        cache.insert("three", 3);
+        assert_eq!(cache.get(&"one"), None);
+        assert_eq!(cache.get(&"two"), Some(&2));
+        assert_eq!(cache.get(&"three"), Some(&3));
+    }
+
+    #[test]
+    fn test_lru_cache() {
+        let mut lru_cache = LRUCache::new(3);
+
+        lru_cache.insert("one", 1);
+        lru_cache.insert("two", 2);
+        lru_cache.insert("three", 3);
+
+        assert_eq!(lru_cache.get(&"two"), Some(&2));
+
+        lru_cache.insert("four", 4);
+
+        assert_eq!(lru_cache.get(&"one"), None);
+        assert_eq!(lru_cache.get(&"two"), Some(&2));
+        assert_eq!(lru_cache.get(&"three"), Some(&3));
     }
 }
